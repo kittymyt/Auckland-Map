e240765cf0452b74169db0c83720166427dda64a
kittymyt <kittymyt@gmail.com>
2016-09-13 23:17:07 +1200

    Add project files and folders

---
+++ script/script.js
@@ -0,0 +1,12 @@
+     function initMap() {
+        // Create a map object and specify the DOM element for display.
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -36.84, lng: 174.76},
+          scrollwheel: false,
+          zoom: 12
+        });
+      }
+
+      
+
+
